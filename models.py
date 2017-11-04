98836d26257ae84994f1649550e5b027d12fdf38
Benoit Gaujac <benoitgaujac@eduroam-int-dhcp-97-72-18.ucl.ac.uk>
2017-11-04 11:10:20 +0000

    fixed typo regu

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 import pdb
 import lasagne.updates as upd
-import lasagne.regularization as regularization
+from lasagne.regularization import regularize_layer_params, l2
 import theano
 import theano.tensor as T
 from theano.tensor.shared_randomstreams import RandomStreams
@@ -43,9 +43,9 @@ def build_model(X, obj_fct, alpha, sampling_method, p_flip,
     # Build loss function, regularization & updates dictionary
     loss, z1, z2 = objectives(X,samples,log_q,energy,E_data,obj_fct,approx_grad=True)
     if obj_fct=='boltzman':
-        l2 = T.sum(T.sqr(params[0]))
+        l2_penalty = T.sum(T.sqr(params[0]))
     else:
-        l2 = regularization.regularize_layer_params(l_out,l2)
+        l2_penalty = regularize_layer_params(l_out,l2)
     L = -loss + coef_regu*l2
     updates = upd.adam(L, params, learning_rate=alpha)
     updates.update(updts) #we need to ad the update dictionary
