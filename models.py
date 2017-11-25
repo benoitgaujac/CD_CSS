06f74a395494ad89e0495e20e7c8c3d2fd312d34
Benoit Gaujac <benoitgaujac@Benoits-MacBook-Pro.local>
2017-11-25 14:21:11 +0000

    fix typo main r42

--- models.py
+++ models.py
@@ -80,7 +80,7 @@ def build_model(X, obj_fct, alpha, sampling_method, p_flip,
     trainloss_function = theano.function(inputs=[X,p_flip], outputs=(loss,z1,logZ,E_samples,logq), updates=updates,on_unused_input='ignore')
     testloss_function = theano.function(inputs=[X,p_flip],
                                         outputs=(loss,z1,logZ,E_samples,logq,
-                                                loss100,logZ1000,E_samples1000,logq1000
+                                                loss100,
                                                 loss500,
                                                 loss1000,logZ1000,E_samples1000,logq1000),
                                         on_unused_input='ignore')
